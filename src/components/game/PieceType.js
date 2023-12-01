7aa7276055c192011ad44097b7ec5e923706abd7
unknown <micaelaestrellamorales@gmail.com>
2023-12-01 11:16:48 -0300

    Fix triangle drag
    Rotate the triangle image to make the center of the figure the dragging
    point

--- src/components/game/PieceType.js
+++ src/components/game/PieceType.js
@@ -2,20 +2,20 @@ const PieceType = {
   STRIANGLE: {
     id: 0,
     src: "./triangle.png",
-    height: 100,
-    width: 100,
+    height: 70.71,
+    width: 141.42,
   },
   MTRIANGLE: {
     id: 1,
     src: "./triangle.png",
-    height: 141.42,
-    width: 141.42,
+    height: 100,
+    width: 200,
   },
   LTRIANGLE: {
     id: 2,
     src: "./triangle.png",
-    height: 200,
-    width: 200,
+    height: 141.42,
+    width: 282.84,
   },
   PARALLELOGRAM: {
     id: 3,
